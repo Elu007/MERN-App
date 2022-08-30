4ecafb264f266b9b933abb38ab5778170a88a18d
Elaf <skelaf2000@gmail.com>
2022-08-30 19:21:27 +0530

    Added cache storing

--- server/router/auth.js
+++ server/router/auth.js
@@ -80,6 +80,11 @@ router.post('/signin', async (req, res) => {
 
             const token = await userLogin.generateAuthToken();
 
+            res.cookie("jwtoken", token, {
+                expires: new Date(Date.now() + 25892000000),
+                httpOnly: true
+            });
+
             if (!isMatch) {
                 res.status(400).json({ error: "Invalid Credientials" });
             } else {
