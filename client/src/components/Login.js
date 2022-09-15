547f5db202fbb69e44f4767b32ec5ac48c5d2fbd
Elaf <skelaf2000@gmail.com>
2022-09-15 21:46:21 +0530

    Added login details JWT Token

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React,{useState} from 'react'
 import login from "../images/login.jpg"
-import { NavLink } from 'react-router-dom'
+import { NavLink, useHistory } from 'react-router-dom'
 
 const Login = () => {
+  const history = useHistory();
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+  const loginUser = async (e) =>{
+    e.preventDefault(0);
+    const res = await fetch('/signin',{
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email,
+        password
+      })
+    });
+    const data = res.json();
+    if(res.status === 400 || !data){
+      window.alert("Invalid Credentials");
+    } else{
+      window.alert("Login success")
+      history.push("/");
+    }
+  }
   return (
     <>
       <section className="sign-in">
@@ -16,21 +39,21 @@ const Login = () => {
               </div>
             <div className="signin-form">
               <h2 className="form-title">Sign up</h2>
-              <form className='register-form' id='register-form'>
+              <form method="POST" className='register-form' id='register-form'>
                 <div className="form-group">
                   <label htmlFor="email">
                     <i className='zmdi zmdi-email material-icons-name'></i>
                   </label>
-                  <input type="email" name='email' id='email' autoCapitalize='off' placeholder='Your Email'/>
+                  <input type="email" name='email' id='email' autoCapitalize='off' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Your Email'/>
                 </div>
                 <div className="form-group">
                   <label htmlFor="password">
                     <i className='zmdi zmdi-lock material-icons-name'></i>
                   </label>
-                  <input type="password" name='password' id='password' autoCapitalize='off' placeholder='Your Password'/>
+                  <input type="password" name='password' id='password' autoCapitalize='off' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Your Password'/>
                 </div>
                 <div className="from-group from-button">
-                  <input type="submit" name='signin' id='signin' className='form-submit btn btn-primary' value="Log in" />
+                  <input type="submit" name='signin' id='signin' className='form-submit btn btn-primary' value="Log in" onClick={loginUser}/>
                 </div>
               </form>
             </div>
