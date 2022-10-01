1e0056b969955e5304f47aee7e6ec595c1c1aeaa
Elaf <skelaf2000@gmail.com>
2022-10-01 21:12:46 +0530

    make it better

--- server/app.js
+++ server/app.js
@@ -12,7 +12,8 @@ app.use(express.json());
 // To link router files we need this middleware
 app.use(require('./router/auth'));
 
-const PORT = process.env.PORT;
+// 2nd step to host in Heroku
+const PORT = process.env.PORT || 5000;
 
 // app.get('/contact', (req,res)=>{
 //     res.send(`Hello World, this is elaf a MERN Stack dev, contact me`);
@@ -23,6 +24,11 @@ app.get('/signin', (req,res)=>{
 app.get('/signup', (req,res)=>{
     res.send(`Hello World, this is elaf a MERN Stack dev, signup page`);
 });
+
+// 3rd step to host in Heroku
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+}
 
 app.listen(PORT, ()=>{
     console.log(`server is running at ${PORT}`)
